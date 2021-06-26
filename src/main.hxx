9da4181c0bf25e3593aea10487ae127a9f3b769d
Subhajit Sahu <wolfram77@gmail.com>
2021-06-26 15:34:48 +0530

    Initial commit

---
+++ src/main.hxx
@@ -0,0 +1,12 @@
+#pragma once
+#include "_main.hxx"
+#include "DiGraph.hxx"
+#include "vertices.hxx"
+#include "edges.hxx"
+#include "mtx.hxx"
+#include "copy.hxx"
+#include "transpose.hxx"
+#include "components.hxx"
+#include "topologicalSort.hxx"
+#include "pagerank.hxx"
+#include "pagerankSeq.hxx"
