aa506665078e2661e4b6d9ec148109dc85f1a99f
Subhajit Sahu <wolfram77@gmail.com>
2021-12-06 22:35:05 +0530

    :turtle: w/ expt

--- main.cxx
+++ main.cxx
@@ -9,22 +9,22 @@ using namespace std;
 
 
 template <class G, class H>
-void runPagerank(const G& x, const H& xt, bool show) {
-  int repeat = 5;
-  bool skipInidenticals = true;
+void runPagerank(const G& x, const H& xt, int repeat) {
+  enum NormFunction { L0=0, L1=1, L2=2, Li=3 };
   vector<float> *init   = nullptr;
+  bool skipInidenticals = true;
 
   // Find in-identicals.
   auto is = inIdenticals(x, xt);
   printf("inidenticals: %d inidentical-groups: %d {}\n", size2d(is), size(is));
 
   // Find pagerank without optimization.
-  auto a1 = pagerankSeq(x, xt, init, {repeat});
+  auto a1 = pagerankMonolithicSeq(x, xt, init, {repeat, L1});
   auto e1 = l1Norm(a1.ranks, a1.ranks);
   printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankSeq\n", a1.time, a1.iterations, e1);
 
   // Find pagerank skipping rank calculation of in-identical vertices.
-  auto a2 = pagerankSeq(x, xt, init, {repeat, skipInidenticals});
+  auto a2 = pagerankMonolithicSeq(x, xt, init, {repeat, L1, skipInidenticals});
   auto e2 = l1Norm(a2.ranks, a1.ranks);
   printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankSeq [skip]\n", a2.time, a2.iterations, e2);
 }
@@ -32,11 +32,11 @@ void runPagerank(const G& x, const H& xt, bool show) {
 
 int main(int argc, char **argv) {
   char *file = argv[1];
-  bool  show = argc > 2;
+  int repeat = argc>2? stoi(argv[2]) : 5;
   printf("Loading graph %s ...\n", file);
   auto x  = readMtx(file); println(x);
   auto xt = transposeWithDegree(x); print(xt); printf(" (transposeWithDegree)\n");
-  runPagerank(x, xt, show);
+  runPagerank(x, xt, repeat);
   printf("\n");
   return 0;
 }
